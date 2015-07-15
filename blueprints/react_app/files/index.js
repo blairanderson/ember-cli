f9969e11296a6e99ecdc9ffc1bef33dcc0993daf
Blair Anderson <Blair81@gmail.com>
2015-07-15 00:38:46 -0700

    update package.son and react blueprint to ignore sidebar

--- blueprints/react_app/files/index.js
+++ blueprints/react_app/files/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import App from './containers/App';
+//import App from './containers/App';
+import App from './containers/TodoApp';
 import 'todomvc-app-css/index.css';
 
 React.render(
